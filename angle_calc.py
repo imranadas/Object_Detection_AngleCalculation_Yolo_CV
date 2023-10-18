cee3b2a98d437874d77fae7d1a54a6cd237aeba5
EntropYgg <ranadas23112002@gmail.com>
2023-10-19 04:06:39 +0530

    Something Worked

--- angle_calc.py
+++ angle_calc.py
@@ -1,7 +1,6 @@
 from ultralytics import YOLO
 import cv2
 from detection import detection_tensors
-import numpy as np
 
 src = cv2.imread("test.jpg")
 detections = detection_tensors(src)
@@ -14,21 +13,10 @@ for detection in detections:
         y1 = int(y1.item())
         x2 = int(x2.item())
         y2 = int(y2.item())
-        print (x1, x2, y1, y2)
-    '''
         subimage = src[y1:y2, x1:x2]
         grayscale_subimage = cv2.cvtColor(subimage, cv2.COLOR_BGR2GRAY)
         canny_edges = cv2.Canny(grayscale_subimage, 50, 150)
         median_blurred_edges = cv2.medianBlur(canny_edges, 3)
-        thresholded_edges = cv2.threshold(median_blurred_edges, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-        mask = np.zeros(thresholded_edges.shape, dtype=np.uint8)
-        mask[y1 - y1:y2 - y1, x1 - x1:x2 - x1] = 255
-        masked_edges = cv2.bitwise_and(thresholded_edges, mask)
-        masked_edges = cv2.resize(masked_edges, (subimage.shape[1], subimage.shape[0]))
-        highlighted_image = cv2.addWeighted(subimage, 1.0, cv2.cvtColor(masked_edges, cv2.COLOR_GRAY2BGR), 0.5, 0)
-        cv2.imshow('Image with Edges', highlighted_image)
-
+        thresholded_edges = cv2.threshold(median_blurred_edges, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]      
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-'''
