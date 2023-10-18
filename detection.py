904381f8668b9b48e32494dc711a2c0a2edb2d8e
EntropYgg <ranadas23112002@gmail.com>
2023-10-19 04:45:05 +0530

    CleanUp

--- detection.py
+++ detection.py
@@ -1,5 +1,5 @@
 def detection_tensors(src):
     from ultralytics import YOLO
-    model = YOLO("book_yolov8.pt") # Loading Trained Model
+    model = YOLO("#model") # Loading Trained Model
     results = model.predict(src, conf = 0.65) # Inference
     return results
