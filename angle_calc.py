a9f2164b43828d7cc6ea7391746732098dd08f2c
EntropYgg <ranadas23112002@gmail.com>
2023-10-19 03:15:39 +0530

    working on edge detection

--- angle_calc.py
+++ angle_calc.py
@@ -1,23 +1,29 @@
 from ultralytics import YOLO
-from PIL import Image
 import cv2
 from detection import detection_tensors
-import torch
+import numpy as np
 
 src = "test.jpg"
 detections = detection_tensors(src)
+
 for detection in detections:
     data = detection.boxes.data.clone().detach()
     for subimage_parameters in data:
         x1, y1, x2, y2, score, label = subimage_parameters
-        x1 = x1.item()
-        y1 = y1.item()
-        x2 = x2.item()
-        y2 = y2.item()
+        x1 = int(x1.item())
+        y1 = int(y1.item())
+        x2 = int(x2.item())
+        y2 = int(y2.item())
         subimage = src[y1:y2, x1:x2]
-        edges = cv2.Canny(subimage, threshold1=30, threshold2=100)
-        image_with_edges = cv2.bitwise_and(src, src, mask=edges)
+        grayscale_subimage = cv2.cvtColor(subimage, cv2.COLOR_BGR2GRAY)
+        canny_edges = cv2.Canny(grayscale_subimage, 50, 150)
+        median_blurred_edges = cv2.medianBlur(canny_edges, 3)
+        thresholded_edges = cv2.threshold(median_blurred_edges, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+        mask = np.zeros(src.shape, dtype=np.uint8)
+        mask[y1:y2, x1:x2] = 255
+        masked_edges = cv2.bitwise_and(thresholded_edges, mask)
+        highlighted_image = cv2.addWeighted(src, 1.0, masked_edges, 0.5, 0)
 
-cv2.imshow('Image with Edges', image_with_edges)
+cv2.imshow('Image with Edges', highlighted_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
